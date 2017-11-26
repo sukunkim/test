48c5324099419ccdae13ce8cfdcefab58da579b4
Sukun Kim <sukun.kim@gmail.com>
2017-11-26 14:33:29 +0900

    Changed TestDBCP.

--- etc/dbcp/TestDBCP.java
+++ etc/dbcp/TestDBCP.java
@@ -1,7 +1,38 @@
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.apache.commons.dbcp2.BasicDataSource;
+
+
 public class TestDBCP {
+
+  private BasicDataSource ds;
 
 
   private TestDBCP() {
+    ds = new BasicDataSource();
+    ds.setUrl("jdbc:mysql://124.137.25.50:23306/test_data");
+    ds.setUsername("data_server");
+    ds.setPassword("Sinbinet12#$");
+
+    ds.setMinIdle(5);
+    ds.setMaxIdle(10);
+    ds.setMaxOpenPreparedStatements(100);
+
+    try {
+      Connection connection = ds.getConnection();
+      PreparedStatement ps = connection.prepareStatement(
+        "SELECT * FROM site");
+
+      ResultSet rs = ps.executeQuery();
+      while (rs.next()) {
+        System.out.println(rs.getInt(1) + "," + rs.getString(2));
+      }
+
+    } catch (SQLException e) {
+    }
   }
 
   public static TestDBCP getInstance() {
