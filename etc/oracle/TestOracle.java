9479158f70ddb753ca4cb1cac11df970398c36bb
Sukun Kim <sukun.kim@gmail.com>
2017-12-01 10:14:56 +0900

    Changed TestOracle.

--- etc/oracle/TestOracle.java
+++ etc/oracle/TestOracle.java
@@ -1,2 +1,47 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class TestOracle {
+
+  private TestOracle() {
+    try {
+      Class.forName("oracle.jdbc.OracleDriver");
+
+    } catch (ClassNotFoundException e) {
+      System.out.println("ClassNotFoundException = " + e);
+    }
+
+    try {
+      Connection conn = DriverManager.getConnection(
+        "",
+        "",
+        "");
+
+      Statement stmt = conn.createStatement();
+      ResultSet rs = stmt.executeQuery("SELECT * FROM location_update");
+
+      while (rs.next()) {
+        System.out.println("MobileId = " + rs.getInt(1));
+      }
+
+      conn.close();
+
+    } catch (SQLException e) {
+      System.out.println("SQLException = " + e);
+    }
+  }
+
+  public static TestOracle getInstance() {
+    TestOracle testOracle = new TestOracle();
+
+    return testOracle;
+  }
+
+
+  public static void main(String[] args) {
+    TestOracle.getInstance();
+  }
 }
