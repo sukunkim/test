871e3e493bcc0466e789bb74b0c3ea9995e33a3a
Sukun Kim <sukun.kim@gmail.com>
2018-02-26 10:36:00 +0900

    JUnit test is modified.

--- etc/junit/JUnit5Tests.java
+++ etc/junit/JUnit5Tests.java
@@ -1,8 +1,12 @@
+import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assumptions.assumeTrue;
 
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class JUnit5Tests {
 
@@ -18,11 +22,22 @@ public class JUnit5Tests {
     assertEquals(2, 1 + 3);
   }
 
+  @Test
+  void groupedAssertions() {
+    assertAll(() -> assertEquals(2, 1), () -> assertEquals(2, 3));
+  }
+
   @Nested
   class TimerTest {
     @Test
     void test11() {
       assertEquals(3, 1 + 2);
     }
+  }
+
+  @ParameterizedTest
+  @ValueSource(ints = {6, 7, 8})
+  void findById(Integer id) {
+    assertEquals(id, id);
   }
 }
