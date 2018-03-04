fafa313fc1864b26fc16687ee08d011d12a152d2
Sukun Kim <sukun.kim@gmail.com>
2018-03-04 14:33:18 +0900

    Added dynamic test examples.

--- etc/junit/JUnit5Tests.java
+++ etc/junit/JUnit5Tests.java
@@ -1,20 +1,30 @@
 import java.time.LocalDate;
 import java.util.Arrays;
+import java.util.Collection;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Random;
 import java.util.concurrent.TimeUnit;
+import java.util.function.Function;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import static org.junit.jupiter.api.Assumptions.assumeTrue;
 
+import static org.junit.jupiter.api.DynamicContainer.dynamicContainer;
+import static org.junit.jupiter.api.DynamicTest.dynamicTest;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.DynamicTest;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.RepetitionInfo;
@@ -34,6 +44,8 @@ import org.junit.jupiter.api.condition.JRE;
 import org.junit.jupiter.api.condition.OS;
 
 import org.junit.jupiter.api.extension.ExtensionContext;
+
+import org.junit.jupiter.api.function.ThrowingConsumer;
 
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.converter.ArgumentConverter;
@@ -237,6 +249,91 @@ class ConversionTests {
 
 
 class DynamicFactoryTests {
+  @TestFactory
+  List<String> dynamicTestsWithInvalidReturnType() {
+    return Arrays.asList("Hello");
+  }
+
+  @TestFactory
+  Collection<DynamicTest> dynamicTestsFromCollection() {
+    return Arrays.asList(
+      dynamicTest("1st dynamic test", () -> assertTrue(true)),
+      dynamicTest("2nd dynamic test", () -> assertEquals(4, 2 * 2))
+    );
+  }
+
+  @TestFactory
+  Iterable<DynamicTest> dynamicTestsFromIterable() {
+    return Arrays.asList(
+      dynamicTest("3rd dynamic test", () -> assertTrue(true)),
+      dynamicTest("4th dynamic test", () -> assertEquals(4, 2 * 2))
+    );
+  }
+
+  @TestFactory
+  Iterator<DynamicTest> dynamicTestsFromIterator() {
+    return Arrays.asList(
+      dynamicTest("5th dynamic test", () -> assertTrue(true)),
+      dynamicTest("6th dynamic test", () -> assertEquals(4, 2 * 2))
+    ).iterator();
+  }
+
+  @TestFactory
+  Stream<DynamicTest> dynamicTestFromStream() {
+    return Stream.of("A", "B", "C")
+      .map(str -> dynamicTest("test" + str, () -> { assertEquals(str, str); }));
+  }
+
+  @TestFactory
+  Stream<DynamicTest> dynamicTestFromIntStream() {
+    return IntStream.iterate(0, n -> n + 2).limit(10)
+      .mapToObj(n -> dynamicTest("test" + n, () -> assertTrue(n % 2 == 0)));
+  }
+
+  @TestFactory
+  Stream<DynamicTest> generateRandomNumberOfTests() {
+    Iterator<Integer> inputGenerator = new Iterator<Integer>() {
+      Random random = new Random();
+      int current;
+
+      @Override
+      public boolean hasNext() {
+        current = random.nextInt(100);
+	return current % 7 != 0;
+      }
+
+      @Override
+      public Integer next() {
+        return current;
+      }
+    };
+
+    Function<Integer, String> displayNameGenerator = (input) -> "input:" + input;
+
+    ThrowingConsumer<Integer> testExecutor = (input) -> assertTrue(input % 7 != 0);
+
+    return DynamicTest.stream(inputGenerator, displayNameGenerator, testExecutor);
+  }
+
+  @TestFactory
+  Stream<DynamicTest> dynamicTestsWithSimpleStream() {
+    return Stream.of(
+      dynamicTest("length > 0", () -> assertTrue(1 > 0)),
+      dynamicTest("non empty", () -> assertFalse("D".isEmpty()))
+    );
+  }
+
+  @TestFactory
+  Stream<DynamicTest> dynamicTestsWithContainers() {
+    return Stream.of("A", "B", "C")
+      .map(input -> //dynamicContainer("Container " + input, Stream.of(
+        //dynamicTest("not null", () -> assertNotNull(input)),
+	dynamicContainer("properties", Stream.of(
+	  dynamicTest("length > 0", () -> assertTrue(input.length() > 0)),
+	  dynamicTest("non empty", () -> assertFalse(input.isEmpty()))
+	  //))
+	)));
+  }
 }
 
 
