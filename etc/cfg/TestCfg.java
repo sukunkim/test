56c7abc00a473d53ac277a65f31f8e0840b99578
Sukun Kim <sukun.kim@gmail.com>
2018-03-10 21:13:28 +0900

    Added configuration with XSD.

---
+++ etc/cfg/TestCfg.java
@@ -0,0 +1,57 @@
+import org.apache.commons.configuration2.Configuration;
+import org.apache.commons.configuration2.XMLConfiguration;
+
+import org.apache.commons.configuration2.builder.FileBasedConfigurationBuilder;
+import org.apache.commons.configuration2.builder.fluent.Parameters;
+
+import org.apache.commons.configuration2.ex.ConfigurationException;
+import org.apache.commons.configuration2.ex.ConversionException;
+
+import org.apache.commons.configuration2.resolver.CatalogResolver;
+
+
+public class TestCfg {
+
+  private TestCfg() {
+    Parameters params = new Parameters();
+    CatalogResolver resolver = new CatalogResolver();
+    resolver.setCatalogFiles("mycfg.xsd");
+
+    FileBasedConfigurationBuilder<XMLConfiguration> builder
+      = new FileBasedConfigurationBuilder<XMLConfiguration>(
+        XMLConfiguration.class)
+	.configure(params.xml().setFileName("mycfg.xml")
+	  .setEntityResolver(resolver).setSchemaValidation(true));
+
+    XMLConfiguration cfg = null;
+    try {
+      cfg = builder.getConfiguration();
+
+    } catch (ConfigurationException e) {
+      System.out.println("ConfigurationException = " + e);
+      return;
+    }
+
+    int port = 0;
+    try {
+      port = cfg.getInt("port");
+
+    } catch (ConversionException e) {
+      System.out.println("ConversionException = " + e);
+      return;
+    }
+
+    System.out.println("port = " + port);
+  }
+
+  public static TestCfg getInstance() {
+    TestCfg testCfg = new TestCfg();
+
+    return testCfg;
+  }
+
+
+  public static void main(String[] args) {
+    TestCfg.getInstance();
+  }
+}
