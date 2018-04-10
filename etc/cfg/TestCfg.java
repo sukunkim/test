5016ad25cd6b5a75570a172b7805382a1b990a82
Sukun Kim <sukun.kim@gmail.com>
2018-04-10 16:36:57 +0900

    Changed configuration test.

--- etc/cfg/TestCfg.java
+++ etc/cfg/TestCfg.java
@@ -9,32 +9,17 @@ import org.apache.commons.configuration2.builder.fluent.Parameters;
 import org.apache.commons.configuration2.ex.ConfigurationException;
 import org.apache.commons.configuration2.ex.ConversionException;
 
-import org.apache.commons.configuration2.resolver.CatalogResolver;
-import org.apache.commons.configuration2.resolver.DefaultEntityResolver;
-
 
 public class TestCfg {
 
   private TestCfg() {
     Parameters params = new Parameters();
 
-    DefaultEntityResolver defaultEntityResolver = new DefaultEntityResolver();
-    URL dtdURL = getClass().getResource("mycfg.dtd");
-    defaultEntityResolver.registerEntityId(
-      "-//Sun Microsystems, Inc.//DTD Web Application 2.2//EN", dtdURL);
-
-    /*
-    CatalogResolver resolver = new CatalogResolver();
-    resolver.setCatalogFiles("mycfg.xsd");
-    */
-
     FileBasedConfigurationBuilder<XMLConfiguration> builder
       = new FileBasedConfigurationBuilder<XMLConfiguration>(
         XMLConfiguration.class)
 	.configure(params.xml().setFileName("mycfg.xml")
-	  .setEntityResolver(defaultEntityResolver).setValidating(true));
-//	  .setEntityResolver(resolver).setSchemaValidation(true));
-//	  .setValidating(true));
+	.setSchemaValidation(true));
 
     XMLConfiguration cfg = null;
     try {
