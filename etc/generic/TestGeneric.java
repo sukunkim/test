5d90104ac9b08af86b791f44ac96271648f556c6
Sukun Kim <sukun.kim@gmail.com>
2018-04-11 21:02:48 +0900

    Added a test for generic.

---
+++ etc/generic/TestGeneric.java
@@ -0,0 +1,12 @@
+import net.tinyos.xtool.data.*;
+
+import net.tinyos.ylocalization.data.*;
+
+public class TestGeneric {
+  public static <T extends Tuple & Comparable<T>>
+    void syncMem(T[] in, T[] out) {
+  }
+
+  public static void main(String[] args) {
+  }
+}
